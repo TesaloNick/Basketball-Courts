4be2f23b4b94ab79e163637cbaf7c619bd8236f5
Nikolay Antonovich <TesaloNick@gmail.com>
2022-09-02 16:50:35 +0300

    added second input

--- js/index.js
+++ js/index.js
@@ -3,29 +3,29 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidGVzYWxvbmljayIsImEiOiJjbDdodmdheHIwaWIyM3Vtb
 const urlBase = 'https://api.mapbox.com/isochrone/v1/mapbox/';
 const lon = 27.55689040736249;
 const lat = 53.89733609094718;
-let profile = 'walking';
-let minutes = 10;
+let profile = 'cycling';
+let minutes = 5;
 let coordinates = ''
 
 const map = new mapboxgl.Map({
   container: 'map', // container id
   style: 'mapbox://styles/mapbox/streets-v11', // stylesheet
-  center: [lon, lat], // starting position [lng, lat]
-  zoom: 11.5 // starting zoom
+  center: [lon, lat],
+  zoom: 10
 });
 
 // const params = document.getElementById('params');
 
-const marker = new mapboxgl.Marker({
-  'color': '#314ccd'
-})
-  .setLngLat([lon, lat]) // Marker [lng, lat] coordinates
-  .addTo(map); // Add the marker to the map;
+// const marker = new mapboxgl.Marker({
+//   'color': 'red'
+// })
+// .setLngLat([lon, lat]) // Marker [lng, lat] coordinates
+// .addTo(map); // Add the marker to the map;
 
-const lngLat = {
-  lon: lon,
-  lat: lat
-};
+// const lngLat = {
+//   lon: lon,
+//   lat: lat
+// };
 
 async function getIso(lat, lon) {
   const query = await fetch(
@@ -33,6 +33,7 @@ async function getIso(lat, lon) {
     { method: 'GET' }
   );
   const data = await query.json();
+  // console.log(data.features[0].geometry.coordinates[0]);
   console.log(data);
   await map.getSource('iso').setData(data);
 }
@@ -46,21 +47,34 @@ async function getIso(lat, lon) {
 //   getIso();
 // });
 
-const geocoder = new MapboxGeocoder({
+const geocoderFrom = new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken, // Set the access token
+  // mapboxgl: mapboxgl, // Set the mapbox-gl instance
+  marker: true, // Do not use the default marker style
+  placeholder: 'From', // Placeholder text for the search bar
+  // bbox: [-122.30937, 37.84214, -122.23715, 37.89838], // Boundary for Berkeley
+  // proximity: {
+  // longitude: 27.55689040736249,
+  // latitude: 53.89733609094718
+  // }
+});
+
+const geocoderWhere = new MapboxGeocoder({
   accessToken: mapboxgl.accessToken, // Set the access token
   mapboxgl: mapboxgl, // Set the mapbox-gl instance
   marker: true, // Do not use the default marker style
-  placeholder: 'Search your place', // Placeholder text for the search bar
+  placeholder: 'Where', // Placeholder text for the search bar
   // bbox: [-122.30937, 37.84214, -122.23715, 37.89838], // Boundary for Berkeley
-  proximity: {
-    // longitude: 27.55689040736249,
-    // latitude: 53.89733609094718
-  }
+  // proximity: {
+  // longitude: 27.55689040736249,
+  // latitude: 53.89733609094718
+  // }
 });
 
-map.addControl(geocoder);
+map.addControl(geocoderFrom);
+map.addControl(geocoderWhere);
 
-map.on('load', async () => {
+map.on('load', () => {
   map.addSource('iso', {
     type: 'geojson',
     data: {
@@ -102,27 +116,13 @@ map.on('load', async () => {
   })
 
 
-  marker.setLngLat(lngLat).addTo(map);
+  // marker.setLngLat(lngLat).addTo(map);
 
-  await geocoder.on('result', async (event) => {
-    await map.getSource('single-point').setData(event.result.geometry)
-    coordinates = await event.result.geometry.coordinates
-    await getIso(coordinates[1], coordinates[0]);
+  geocoderFrom.on('result', (event) => {
+    // await map.getSource('single-point').setData(event.result.geometry)
+    coordinates = event.result.geometry.coordinates
+    getIso(coordinates[1], coordinates[0]);
   });
 
-  await getIso(lat, lon);
+  getIso(lat, lon);
 });
-
-// async function getCoordinates() {
-//   let url = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${coordinates[0]},${coordinates[1]}.json?proximity=ip&types=place%2Cpostcode%2Caddress&access_token=${mapboxgl.accessToken}`)
-//     .then(res => res.json())
-//     .then(res => console.log(res))
-//   // .then(res => res.features.map(item => console.log(item.geometry.coordinates)))
-//   // console.log(url);
-
-//   // let coordinates = await fetch(url)
-//   //   .then(res => res)
-//   //   .then(res => console.log(res))
-
-// }
-// getCoordinates()
